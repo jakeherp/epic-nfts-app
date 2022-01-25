2301a57a4e1a8c28fc613905e84edc0d0a24c765
Jacob Herper <jacob@herper.uk>
2022-01-25 21:12:05 +0000

    adds styling

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,12 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "My Epic NFT Collection" };
 };
 
 export default function App() {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,13 @@
+import styles from "../styles/index.css";
+
 const TWITTER_HANDLE = "jakeherp";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function Index() {
   const renderNotConnectedContainer = () => (
@@ -11,22 +17,24 @@ export default function Index() {
   );
 
   return (
-    <div className="container">
-      <div className="header-container">
-        <p className="header gradient-text">My Epic NFT Collection</p>
-        <p className="sub-text">
-          Each unique. Each beautiful. Discover your NFT today.
-        </p>
-        {renderNotConnectedContainer()}
-      </div>
-      <div className="footer-container">
-        <img alt="Twitter" className="twitter-logo" src="/twitter.svg" />
-        <a
-          className="footer-text"
-          href={TWITTER_LINK}
-          target="_blank"
-          rel="noreferrer"
-        >{`built by @${TWITTER_HANDLE}`}</a>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <h1 className="header gradient-text">My Epic NFT Collection</h1>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter" className="twitter-logo" src="/twitter.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
     </div>
   );
